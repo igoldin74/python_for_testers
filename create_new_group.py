66b71a16f6d640980298bc03e76838566fad9602
igoldin74 <33078752+igoldin74@users.noreply.github.com>
2019-07-08 13:01:56 -0500

    Added parametrized "group" object
    Also updated gitignore

--- create_new_group.py
+++ create_new_group.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import unittest
+from group import Group
 
 
 class NewGroupCreation(unittest.TestCase):
@@ -12,23 +13,23 @@ class NewGroupCreation(unittest.TestCase):
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_group_page(wd)
-        self.create_group(wd, name="test_group_0", header="dfdsfsdfg", footer="sdgdsfdsf")
+        self.create_group(wd, Group(name="test_group_0", header="dfdsfsdfg", footer="sdgdsfdsf"))
         self.open_group_page(wd)
         self.logout(wd)
 
     def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
 
-    def create_group(self, wd, name, header, footer):
+    def create_group(self, wd, group):
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(name)
+        wd.find_element_by_name("group_name").send_keys(group.name)
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(header)
+        wd.find_element_by_name("group_header").send_keys(group.header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(footer)
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
         wd.find_element_by_name("submit").click()
 
     def open_group_page(self, wd):
