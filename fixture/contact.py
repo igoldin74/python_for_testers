26cac734af19d282dbc43b120602c2fd65325ef5
igoldin74 <33078752+igoldin74@users.noreply.github.com>
2019-07-29 15:15:34 -0500

    HW#11
    - Fixed test for contact deletion by adding a delay that waits for confirmation dialog to show up

--- fixture/contact.py
+++ fixture/contact.py
@@ -19,6 +19,7 @@ class ContactHelper:
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_xpath("//input[@value='Delete']").click()
         wd.switch_to_alert().accept()
+        wd.find_element_by_css_selector("div.msgbox")
 
     def modify(self, contact):
         wd = self.app.wd
