65e4bcc7d72ce83662981350b08125caa5d358df
igoldin74 <33078752+igoldin74@users.noreply.github.com>
2019-08-09 08:35:48 -0500

    HW#17
    - Added config file(target.json) with app URL and login details)
    - In test fixture implemented reading app URL and login details from config file

--- conftest.py
+++ conftest.py
@@ -1,19 +1,23 @@
 import pytest
+import json
+import os.path
 from fixture.application import Application
 
 fixture = None
-
+target = None
 
 @pytest.fixture
 def app(request):
     global fixture
-    if fixture is None:
-        browser = request.config.getoption('--browser')
-        baseurl = request.config.getoption('--baseurl')
-        fixture = Application(browser=browser, baseurl=baseurl)
-    elif fixture.is_not_valid():
-        fixture = Application()
-    fixture.session.ensure_login(username="admin", password="secret")
+    global target
+    browser = request.config.getoption('--browser')
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as f:
+            target = json.load(f)
+    if fixture is None or fixture.is_not_valid():
+        fixture = Application(browser=browser, baseurl=target["baseUrl"])
+    fixture.session.ensure_login(username=target["username"], password=target["password"])
     return fixture
 
 
@@ -28,4 +32,4 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption('--browser', action='store', default='firefox')
-    parser.addoption('--baseurl', action='store', default='http://192.168.1.22:8080/addressbook/index.php')
+    parser.addoption('--target', action='store', default='target.json')
